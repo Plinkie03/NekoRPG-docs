899980323ca0a15e6e128f3b30da404d1f2369bc
Dark-LYNN <botdevlynn@gmailcom>
2025-01-24 14:31:30 +0100

    UwU

--- components/main/requirementDisplay.tsx
+++ components/main/requirementDisplay.tsx
@@ -1,5 +1,7 @@
+import { Tooltip } from "@/components/ui/tooltip"
 import React from "react";
 import style from '@/styles/item.module.css'
+import Image from "next/image";
 
 interface Skill {
   level: number;
@@ -23,31 +25,43 @@ const RequirementsDisplay: React.FC<Props> = ({ requirements }) => {
   const { skills, level } = requirements;
 
   return (
-    <div className="requirements-container">
-      {/* Display Level Requirement if available */}
-      {level !== undefined && (
-        <div className="requirement-level">
-          <p>Level {level}</p>
-        </div>
-      )}
+    <div className={style.requirements}>
+      <div>
+        <p className={style.nglIRanOutOfClassNames}>Requirements</p>
+      </div>
+      <div className={style.requirementsList}>
+        {/* Display Level Requirement if available */}
+        {level !== undefined && (
+          <Tooltip content={'Requires: "Character level ' + level + '"'} showArrow={true} openDelay={0} closeDelay={0}>
+            <div className={style.theDevIsGayAF}>
+              <div className={style.requirementLevel}>
+                <p className={style.level}>{level}</p>
+                <div className={style.skillImage}>
+                  <Image src={'/' + 'User' + '.svg'} alt="" height={14} width={14}></Image>
+                </div>
+              </div>
+            </div>
+          </Tooltip>
+        )}
 
-      {/* Display Skills Requirements if available */}
-      {skills && skills.length > 0 && (
-        <div className="requirement-skills">
-          <ul>
-            {skills.map((skill, index) => (
-              <li key={index}>
-                <strong>{skill.type}</strong>: Level {skill.level}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-
-      {/* If no requirements are available */}
-      {!level && (!skills || skills.length === 0) && (
-        <div>No specific requirements for this item.</div>
-      )}
+        {/* Display Skills Requirements if available */}
+        {skills && skills.length > 0 && (
+          <>
+          {skills.map((skill, index) => (
+            <div className={style.theDevIsGayAF} key={index}>
+              <Tooltip content={'Requires: "' + skill.type + ' level ' + skill.level + '"'} showArrow={true} openDelay={0} closeDelay={0}>
+                <div className={style.requirementLevel}>
+                  <p className={style.level}>{skill.level}</p>
+                  <div className={style.skillImage}>
+                    <Image src={'/' + skill.type + '.svg'} alt="" height={14} width={14}></Image>
+                  </div>
+                </div>
+              </Tooltip>
+            </div>
+          ))}
+          </>
+        )}
+      </div>
     </div>
   );
 };
